c429ea4439c6eef485cfe2634e3444a88860a576
jatindergill047@gmail.com <jatindergill047@gmail.com>
2018-11-29 19:22:16 -0700

    set up

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+// // * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// // PATHS
+// // * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// const path = {
+//     sass: './sass',
+//     css: './css'
+// }
+
+
+
+// // * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// // Code
+// // * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// const gulp          = require('gulp')
+// const sass          = require('gulp-sass')
+// const allSassFiles  = path.sass + '/**/*.scss'
+
+// gulp.task('style', () => {
+//     gulp.src(path.sass + '/app.scss')
+//         .pipe(sass())
+//         .pipe(gulp.dest(path.css))
+// })
+
+// gulp.task('watch', () => {
+//     gulp.watch(allSassFiles, ['style'])
+// })
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+      server: {
+        proxy: 'file:///D:/jatinder%20singh/Portfolio/MyPortfolio/index.html'
+      },
+    })
+  })
+
+gulp.task('style', function(){
+    gulp.src('./sass/app.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+}) 
+
+gulp.task('watch', ['browserSync', 'style'], function(){
+    gulp.watch('./sass/**/*.scss', ['style'])
+})
+
+gulp.task('default', ['browserSync', 'style', 'watch']);
