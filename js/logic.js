b4fcdb9895010afbc29af99463f069d0f01a9e9a
jatindergill047@gmail.com <jatindergill047@gmail.com>
2018-12-06 19:57:53 -0700

    compeleting home page

--- js/logic.js
+++ js/logic.js
@@ -2,7 +2,11 @@
 //     document.getElementById("mobile-links").classList.toggle("active");
 
 // }
+AOS.init({
+    duration: 2000,
+});
 $("#wrapper").click( function() {
 	$(".mobile-links").toggleClass("active");
 	$(".menu").toggleClass("close");
 });
+
